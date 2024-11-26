cea7be7da28cc26510dffffbccff5a4ecf7596e6
David <daviddoherty2016@gmail.com>
2024-11-26 14:58:56 +0000

    Day One Complete and Day two started

--- dayOne/dayOneB.c
+++ dayOne/dayOneB.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +10,7 @@
 int parser(char *inputArr) {
 	int total = 0;
 	int index = 0;
-	char *currentLine = malloc((3 + 1) * sizeof(char));
+	char *currentLine = malloc((10 + 1) * sizeof(char));
 	if (!currentLine) {
 		fprintf(stderr, "Memory allocation failed\n");
 		return -1;
@@ -19,26 +20,66 @@ int parser(char *inputArr) {
 
 	for (int i = 0; i < inputLength; i++) {
 		if (isdigit(inputArr[i])) {
-			currentLine[index] = inputArr[i];
-			index++;
+			if (index < 10) {  // Avoid buffer overflow
+				currentLine[index] = inputArr[i];
+				index++;
+			}
 		} else if (!isdigit(inputArr[i])) {
 			switch (inputArr[i]) {
 				case 'o':
-					char *result = strstr(inputArr, "ne");
+					if (strncmp(&inputArr[i + 1], "ne", 2) == 0) {
+						currentLine[index] = '1';
+						index++;
+					}
+					break;
+				case 't':
+					if (strncmp(&inputArr[i + 1], "wo", 2) == 0) {
+						currentLine[index] = '2';
+						index++;
+					} else if (strncmp(&inputArr[i + 1], "hree", 4) == 0) {
+						currentLine[index] = '3';
+						index++;
+					}
+					break;
+				case 'f':
+					if (strncmp(&inputArr[i + 1], "our", 3) == 0) {
+						currentLine[index] = '4';
+						index++;
+					} else if (strncmp(&inputArr[i + 1], "ive", 3) == 0) {
+						currentLine[index] = '5';
+						index++;
+					}
+					break;
+				case 's':
+					if (strncmp(&inputArr[i + 1], "ix", 2) == 0) {
+						currentLine[index] = '6';
+						index++;
+					} else if (strncmp(&inputArr[i + 1], "even", 4) == 0) {
+						currentLine[index] = '7';
+						index++;
+					}
+					break;
+				case 'e':
+					if (strncmp(&inputArr[i + 1], "ight", 4) == 0) {
+						currentLine[index] = '8';
+						index++;
+					}
+					break;
+				case 'n':
+					if (strncmp(&inputArr[i + 1], "ine", 3) == 0) {
+						currentLine[index] = '9';
+						index++;
+					}
+					break;
 			}
+		}
 
-		} else if (inputArr[i] == '\n') {
-			if (index > 0) {
-				currentLine[1] = currentLine[index - 1];
-			} else {
-				currentLine[1] = currentLine[0];
-			}
+		if (inputArr[i] == '\n' && index > 0) {
+			currentLine[1] = currentLine[index - 1];
 			currentLine[2] = '\0';
 			total += atoi(currentLine);
 			memset(currentLine, 0, (3 + 1) * sizeof(char));
 			index = 0;
-		} else {
-			continue;
 		}
 	}
 
@@ -48,7 +89,7 @@ int parser(char *inputArr) {
 
 int main(void) {
 	clock_t start = clock();
-	char *fileName = "inputData.txt";
+	char *fileName = "dayOneA.txt";
 	FILE *fp = fopen(fileName, "r");
 	if (fp == NULL) {
 		printf("Could Not Open File %s\n.", fileName);
---
+++ dayTwo/dayTwoA.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lineChecker(char *line) {
+	int lineLength = strlen(&line[7]);
+}
+
+int main(void) {
+	return 0;
+}
